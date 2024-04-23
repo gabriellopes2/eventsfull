c3cad9181f63a3c820c3cde13ac75405ce422950
Gabriel Lopes <gabriel_lopes@solis.com.br>
2024-04-23 13:08:09 -0300

    Checkin e envio de email.

--- eventsfull/app/Http/Controllers/SubscriptionsController.php
+++ eventsfull/app/Http/Controllers/SubscriptionsController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EventsModel;
 use App\Models\SubscriptionModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Mail\MailSender;
+use Illuminate\Support\Facades\Mail;
+
 
 class SubscriptionsController extends Controller
 {
@@ -22,19 +25,69 @@ class SubscriptionsController extends Controller
     public function register(Request $request)
     {
         $data = $request->all();
-        /*$subscription = new SubscriptionModel();
-        $subscription->users_id = $data['users_id'];
-        $subscription->eventos_id = $data['eventos_id'];
-        $subscription->save();*/
+        $users_id = $data['users_id'];
+        $eventos_id = $data['eventos_id'];
+
 
         SubscriptionModel::create([
-            'users_id' => $data['users_id'],
-            'eventos_id' => $data['eventos_id']
-        ]);  
+            'users_id' => $users_id,
+            'eventos_id' => $eventos_id
+        ]);
+
+        $evento = EventsModel::find($eventos_id);
+
+        $details = [
+            'title' => 'Inscrição efetuada com sucesso!',
+            'body' => "Você realizou a inscrição no seguinte evento:<br><h3>$evento->name</h3><br>Você pode acompanhar as suas inscrições em <a href=\"http://eventsfull/subscriptions\">Minhas inscrições</a>."
+        ];
+
+        // Envia e-mail
+        $this->sendMail($details);
         
         return response()->json([
             "message" => "Inscrição realizada!",
             200
         ]);
+    }
+
+    public function getCheckin()
+    {
+        //$user = Auth::user();
+        //$eventos_id = $users->inscricoes->pluck('eventos_id')->toArray();
+        $checkins = SubscriptionModel::select('subscriptions.id as inscricao_id', 'subscriptions.eventos_id as eventos_id', 'subscriptions.updated_at as hora_checkin', 'events.*')
+            ->join('events', 'events.id', '=', 'subscriptions.eventos_id')
+            ->where('subscriptions.checkin', true)
+            ->whereIn('events.id', $eventos_id)
+            ->get();
+
+        return response()->json($checkins);
+    }
+
+    public function checkin(Request $request)
+    {
+        $subscription = SubscriptionModel::find($request->get('id'));
+        $subscription->checkin = true;
+        $subscription->save();
+
+        $event = EventsModel::find($subscription->eventos_id);
+
+        $details = [
+            'title' => 'Checkin realizado com sucesso!',
+            'body' => "Você realizou o checkin no seguinte evento:<br><h3>$event->name</h3><br>Você pode acompanhar os seus checkins em <a href=\"http://eventsfull/checkin\">Meus checkins</a>."
+        ];
+
+        // Envia e-mail
+        $this->sendMail($details);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Checkin realizado com sucesso!',
+            'evento' => $event->name
+        ]);
+    }
+
+    private function sendMail(Array $details)
+    {
+        Mail::to('user_receiver_email@gmail.com')->send(new MailSender($details));
     }
 }
--- eventsfull/routes/api.php
+++ eventsfull/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EventsController;
 use App\Http\Controllers\SubscriptionsController;
+use App\Models\SubscriptionModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,9 @@ Route::get('events', [EventsController::class, 'index']);
 Route::get('events/{id}', [EventsController::class, 'searchEventParticipants']);
 Route::get('subscriptions/{id}', [SubscriptionsController::class, 'searchSubscription']);
 Route::post('susbcriptions', [SubscriptionsController::class, 'register']);
+Route::post('/checkin', function(Request $request) {
+    $inscricaoController = new SubscriptionModel;
+    $checkin = $inscricaoController->checkin($request);
+
+    return response()->json($checkin);
+})->name('api.checkin');
